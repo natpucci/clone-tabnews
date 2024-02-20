17f4b0b4a7e3af8a4ee94fe854521a5b66cbdb4c
Natan Pucci <112439647+natpucci@users.noreply.github.com>
2024-02-20 21:00:03 +0000

    Tentando adicionar a foto do cachorro na página inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function home(){
-    return <img src="/images/fofo.jpeg" alt="Cachorro fofo"></img>
+    return <img src="images/fofo.jpeg" alt="Cachorrinho fofo" type="image/jpeg" style="height: 628px; width: 1360px;"></img>
 }
 
 export default home;
